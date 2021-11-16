7c691cb47ff67aad53ccede34654891cebb4f0d5
hecnaaquino <hecna.aquino@gcnewhorizons.net>
2021-11-16 17:37:15 +0000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProjectComponent } from './project/project.component';
+import { Project1Component } from './project1/project1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectComponent,
+    Project1Component
   ],
   imports: [
     BrowserModule,
